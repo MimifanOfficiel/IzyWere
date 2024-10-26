c7eb82913b59ddab182d3d7dc04df117c1555057
Lila BRANDON <contact@lilabrandon.fr>
2024-10-27 00:03:40 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Izywere',
+  webDir: 'www'
+};
+
+export default config;
